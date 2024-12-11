dcc8d67636cb18e084c174b120f6407fda50f1ae
Max Topan <maxtopan@hotmail.co.uk>
2024-12-11 15:17:49 +0000

    Add GameService.cs, move functionality from Program.cs

--- MaxTopan_GWRFighter/Program.cs
+++ MaxTopan_GWRFighter/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace MaxTopan_GWRFighter
+﻿using MaxTopan_GWRFighter.Characters;
+using MaxTopan_GWRFighter.Services;
+
+namespace MaxTopan_GWRFighter
 {
     internal class Program
-    {        
+    {
         static void Main(string[] args)
         {
+            GameService gs = new GameService();
+
+            Console.WriteLine(
+                @"========================================
+WELCOME TO GUINNESS WORLD RECORD FIGHTER
+========================================
+");
+            
+            Hero hero = gs.CreateHero();
+            
+            Console.WriteLine($"Okay, {hero.Name}. What do you want to do?");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ MaxTopan_GWRFighter/Services/GameService.cs
@@ -0,0 +1,35 @@
+﻿using MaxTopan_GWRFighter.Characters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxTopan_GWRFighter.Services
+{
+    internal class GameService
+    {
+        public Hero CreateHero()
+        {
+            string? name = null;
+            while (String.IsNullOrWhiteSpace(name) || String.IsNullOrEmpty(name))
+            {
+                Console.Write("Please enter a name for your hero: ");
+                name = Console.ReadLine();
+            }
+
+            return new Hero(name);
+        }
+
+        public int GetMenuChoice(int maxChoice)
+        {
+            int choice;
+            do
+            {
+                Console.Write($"Please choose (1 - {maxChoice}): ");
+            } while (int.TryParse(Console.ReadLine(), out choice) && 1 > choice || choice > maxChoice);
+            
+            return choice;
+        }
+    }
+}
