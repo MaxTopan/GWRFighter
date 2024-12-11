4b067af1e9167e197b30277a7ea27187ad30e3c1
Max Topan <maxtopan@hotmail.co.uk>
2024-12-11 20:14:16 +0000

    Add WeaponHelper class

---
+++ MaxTopan_GWRFighter/Utilities/WeaponHelper.cs
@@ -0,0 +1,21 @@
+﻿using MaxTopan_GWRFighter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxTopan_GWRFighter.Utilities
+{
+    public class WeaponHelper
+    {
+        public static List<Type> GetAllWeapons()
+        {
+            return Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => typeof(IWeapon).IsAssignableFrom(t) && t.IsClass && !t.IsAbstract)
+                .ToList();
+        }
+    }
+}
